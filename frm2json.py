f8fd9d795216bfce5f81faacd3db304e63cb8f04
LinZiyuu <linziyu0205@163.com>
2023-02-23 19:57:43 +0800

    封装H2Client类

--- frm2json.py
+++ frm2json.py
@@ -6,11 +6,11 @@ import json
 
 # NO_MISMATCH_CHECK content-length mismatch data-length cause Content_Length_Incomplete-Withbody
 # "315": {"input":
-no_missmatch_check_frm1 = {"<headers-frame-base-2:1>": {":method": "POST", ":scheme": "https", ":path": "/reqid=315", ":authority": "172.17.168.200", "flags": []},
+no_missmatch_check_frm1 = {"<headers-frame-base-2:1>": {":method": "POST", ":scheme": "https", ":path": "/", ":authority": "172.17.168.200", "flags": []},
 "<continuation-frame-pool-1:1>": {"$content-length": "5", "flags": []},
 "<continuation-frame-pool-1:2>": {"content-length": "20", "flags": []},
 "<continuation-frame-base-1:1>": {"some-header": "some-value", "flags": ["EH"]},
-"<data-frame-base-1:1>": {"data": "5\r\nBBBBB\r\n0\r\n\r\n", "flags": ["ES"]}}
+"<data-frame-base-1:1>": {"data": "5\r\nid315\r\n0\r\n\r\n", "flags": ["ES"]}}
 # "mutations": ["Inserting subtree '<continuation-frame-pool-1>' at pos 1 of '<sequence:1>'.",
 # "Inserting subtree '<continuation-frame-pool-1>' at pos 2 of '<sequence:1>'.",
 # "Inserting character '$' at pos 0 of <continuation-frame-pool-1-content-len-header-name:1>."],
@@ -19,12 +19,12 @@ no_missmatch_check_frm1 = {"<headers-frame-base-2:1>": {":method": "POST", ":sch
 #
 # HEADER_AFTER_END_HEADER cause Content_Length_Incomplete-Withoutbody
 # "61": {"input":
-header_after_end_header_frm1 = {"<headers-frame-base-2:1>": {":method": "POST", ":scheme": "https", ":path": "/reqid=61", ":authority": "172.17.168.200", "flags": []},
+header_after_end_header_frm1 = {"<headers-frame-base-2:1>": {":method": "POST", ":scheme": "https", ":path": "/", ":authority": "172.17.168.200", "flags": []},
 "<continuation-frame-pool-1:1>": {"content-length": "5", "flags": []},
 "<continuation-frame-base-1:1>": {"some-header": "some-value", "flags": ["EH"]},
-"<headers-frame-pool-1:1>": {":method": "_POST", ":scheme": "https", ":path": "/reqid=61", ":authority": "172.17.168.200", "content-length": "10\u001b", "flags": ["ES"]},
+"<headers-frame-pool-1:1>": {":method": "_POST", ":scheme": "https", ":path": "/", ":authority": "172.17.168.200", "content-length": "10\u001b", "flags": ["ES"]},
 "<data-frame-base-2:1>": {"data": "0\r\n\r\n", "flags": []},
-"<data-frame-base-1:1>": {"data": "5\r\nBBBBB\r\n0\r\n\r\nBBBBB", "flags": ["ES"]}}
+"<data-frame-base-1:1>": {"data": "5\r\nid061\r\n0\r\n\r\nid061", "flags": ["ES"]}}
 # "mutations": ["Inserting subtree '<headers-frame-pool-1>' at pos 2 of '<sequence:1>'.",
 # "Inserting subtree '<continuation-frame-pool-1>' at pos 1 of '<sequence:1>'.",
 # "Inserting character '_' at pos 0 of <headers-frame-pool-1-method-header-value:1>.",
@@ -33,11 +33,11 @@ header_after_end_header_frm1 = {"<headers-frame-base-2:1>": {":method": "POST", 
 
 # HEADER_AFTER_END_HEADER cause MISSING_LAST_CHUNK
 # "19": {"input":
-header_after_end_header_frm2 = {"<headers-frame-base-2:1>": {":method": "POST", ":scheme": "https", ":path": "/reqid=19", ":authority": "172.17.168.200", "flags": []},
+header_after_end_header_frm2 = {"<headers-frame-base-2:1>": {":method": "POST", ":scheme": "https", ":path": "/", ":authority": "172.17.168.200", "flags": []},
 "<continuation-frame-base-2:1>": {"some-other-header": "some-other-value", "flags": []},
 "<continuation-frame-base-1:1>": {"some-header": "some-value", "flags": ["EH"]},
-"<headers-frame-pool-1:1>": {":method": "POST\u001d", ":scheme": "https", ":path": "/reqid=19", ":authority": "172.17.168.200", "transfer-encoding~": "chunked", "flags": ["EH"]},
-"<data-frame-base-1:1>": {"data": "5\r\nBBBBB\r\n", "flags": ["ES"]}}
+"<headers-frame-pool-1:1>": {":method": "POST\u001d", ":scheme": "https", ":path": "/", ":authority": "172.17.168.200", "transfer-encoding~": "chunked", "flags": ["EH"]},
+"<data-frame-base-1:1>": {"data": "5\r\nid019\r\n", "flags": ["ES"]}}
 # "mutations": ["Inserting subtree '<headers-frame-pool-1>' at pos 3 of '<sequence:1>'.",
 # "Inserting character '~' at pos 17 of <headers-frame-pool-1-transfer-enc-header-name:1>.",
 # "Inserting character '\\x1d' at pos 4 of <headers-frame-pool-1-method-header-value:1>."],
@@ -45,24 +45,24 @@ header_after_end_header_frm2 = {"<headers-frame-base-2:1>": {":method": "POST", 
 
 # MISSING_END_STREAM cause MISS_LAST_CHUNK
 # "203": {"input":
-miss_end_stream_frm1 = {"<headers-frame-base-2:1>": {":method": "POST", ":scheme": "https", ":path": "/reqid=203", ":authority": "172.17.168.200", "flags": []},
+miss_end_stream_frm1 = {"<headers-frame-base-2:1>": {":method": "POST", ":scheme": "https", ":path": "/", ":authority": "172.17.168.200", "flags": []},
 "<continuation-frame-base-2:1>": {"some-other-header": "some-other-value", "flags": []},
 "<continuation-frame-base-1:1>": {"some-header": "some-value", "flags": ["EH"]},
-"<data-frame-base-2:1>": {"data": "5\r\nBBBBB\r\n0\r\n\r\nBBBBB", "flags": []},
-"<padded-headers-frame-pool-1:1>": {":method": "POST\u0011", ":scheme": "https", ":path": "/reqid=203", ":authority": "172.17.168.200", "content-length": "10", "flags": ["P", "ES", "EH"], "padding": "5\r\nBBBBB\r\n0\r\n\r\n"},
-"<data-frame-base-1:1>": {"data": "5\r\nBBBBB\r\n0\r\n\r\n", "flags": ["ES"]}}
+"<data-frame-base-2:1>": {"data": "5\r\nid203\r\n0\r\n\r\nBBBBB", "flags": []},
+"<padded-headers-frame-pool-1:1>": {":method": "POST\u0011", ":scheme": "https", ":path": "/", ":authority": "172.17.168.200", "content-length": "10", "flags": ["P", "ES", "EH"], "padding": "5\r\nBBBBB\r\n0\r\n\r\n"},
+"<data-frame-base-1:1>": {"data": "5\r\nid203\r\n0\r\n\r\n", "flags": ["ES"]}}
 # "mutations": ["Inserting subtree '<padded-headers-frame-pool-1>' at pos 4 of '<sequence:1>'.",
 # "Inserting character '\\x11' at pos 4 of <padded-headers-frame-pool-1-method-header-value:1>."],
 # "responses": ["response-code: \r\nerror: Protocol error\r\nhost_header: 47.96.175.128\r\n\r\n"]},
 
 # MISSING_END_STREAM cause Content_Length_Incomplete-Withbody
 # "365": {"input":
-miss_end_stream_frm2 = {"<headers-frame-base-2:1>": {":method": "POST", ":scheme": "https", ":path": "/reqid=365", ":authority": "172.17.168.200", "flags": []},
+miss_end_stream_frm2 = {"<headers-frame-base-2:1>": {":method": "POST", ":scheme": "https", ":path": "/", ":authority": "172.17.168.200", "flags": []},
 "<continuation-frame-pool-1:1>": {"content-length": "20", "flags": []},
 "<continuation-frame-base-2:1>": {"some-other-header": "some-other-value", "flags": []},
 "<continuation-frame-base-1:1>": {"some-header": "some-value", "flags": ["EH"]},
-"<data-frame-base-2:1>": {"data": "5\r\nBBBBB\r\n", "flags": []},
-"<headers-frame-pool-1:1>": {":method": "GET", ":scheme": "https", ":path": "/reqid=365", ":authority": "172.17.168.200", "-transfer-encoding": "chunked", "flags": ["ES"]},
+"<data-frame-base-2:1>": {"data": "5\r\nid365\r\n", "flags": []},
+"<headers-frame-pool-1:1>": {":method": "GET", ":scheme": "https", ":path": "/", ":authority": "172.17.168.200", "-transfer-encoding": "chunked", "flags": ["ES"]},
 "<data-frame-base-1:1>": {"data": "0\r\n\r\n", "flags": ["ES"]}}
 # "mutations": ["Inserting subtree '<continuation-frame-pool-1>' at pos 1 of '<sequence:1>'.",
 # "Inserting subtree '<headers-frame-pool-1>' at pos 5 of '<sequence:1>'.",
--- h2client.py
+++ h2client.py
@@ -5,223 +5,228 @@ import scapy.contrib.http2 as h2
 import scapy.config
 import scapy.packet as packet
 from gen_frm import extract_type, build_frame, load_data
-
-def tls_setup_exchange(dn, port):
-    l = socket.getaddrinfo(dn, port, socket.INADDR_ANY, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-    assert (len(l) > 0), 'No address found :('
-    # assert('h2' == ssl_sock.selected_alpn_protocol())
-    s = socket.socket(l[0][0], l[0][1], l[0][2])
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    if hasattr(socket, 'SO_REUSEPORT'):
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-    ip_and_port = l[0][4]
-    print("l[0]:{}\nL[1]:{}\nL[2]:{}\nL[3]:{}\nL[4]:{}\n".format(l[0][0], l[0][1], l[0][2], l[0][3], l[0][4]))
-
-    # Building the SSL context
-    ssl_ctx = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-    ssl_ctx.set_ciphers(':'.join([  # List from ANSSI TLS guide v.1.1 p.51
-        'ECDHE-ECDSA-AES256-GCM-SHA384',
-        'ECDHE-RSA-AES256-GCM-SHA384',
-        'ECDHE-ECDSA-AES128-GCM-SHA256',
-        'ECDHE-RSA-AES128-GCM-SHA256',
-        'ECDHE-ECDSA-AES256-SHA384',
-        'ECDHE-RSA-AES256-SHA384',
-        'ECDHE-ECDSA-AES128-SHA256',
-        'ECDHE-RSA-AES128-SHA256',
-        'ECDHE-ECDSA-CAMELLIA256-SHA384',
-        'ECDHE-RSA-CAMELLIA256-SHA384',
-        'ECDHE-ECDSA-CAMELLIA128-SHA256',
-        'ECDHE-RSA-CAMELLIA128-SHA256',
-        'DHE-RSA-AES256-GCM-SHA384',
-        'DHE-RSA-AES128-GCM-SHA256',
-        'DHE-RSA-AES256-SHA256',
-        'DHE-RSA-AES128-SHA256',
-        'AES256-GCM-SHA384',
-        'AES128-GCM-SHA256',
-        'AES256-SHA256',
-        'AES128-SHA256',
-        'CAMELLIA128-SHA256'
-    ]))
-
-    ssl_ctx.set_alpn_protocols(['h2'])  # h2 is a RFC7540-hardcoded value
-    ssl_sock = ssl_ctx.wrap_socket(s, server_hostname=dn)
-
-    # connect the socket to the TCP endpoint.
-    ssl_sock.connect(ip_and_port)
-    # test alpn protocol
-    print("alpn protocol:", ssl_sock.selected_alpn_protocol())
-    assert ('h2' == ssl_sock.selected_alpn_protocol())
-
-    # Reading the server settings and acknowledging them.
-    scapy.config.conf.debug_dissector = True
-    ssl_stream_sock = supersocket.SSLStreamSocket(ssl_sock, basecls=h2.H2Frame)
-    return ssl_stream_sock
+from helper_functions import _print_exception
 
 
-def initial_h2_exchange(sock):
-    # initial_h2_exchange
+class H2Client:
 
-    # SENDING MAGIC
-    magic = packet.Raw(h2.H2_CLIENT_CONNECTION_PREFACE)
-    print("-" * 32 + "sending_magic" + "-" * 32)
-    sock.send(magic)
-    magic.show()
-    print("-" * 32 + "sending_magic" + "-" * 32)
+    def __init__(self, verbose=False):
+        self.verbose = verbose
 
-    # RECEIVING
-
-    srv_set = ssl_stream_sock.recv()
-    print("-" * 32 + "RECEIVING" + "-" * 32)
-    srv_set.show()
-    print("-" * 32 + "RECEIVING" + "-" * 32)
-
-    # 服务器的设置 根据RFC 有默认值
-    srv_max_frm_sz = 1 << 14
-    srv_hdr_tbl_sz = 4096
-    srv_max_hdr_tbl_sz = 0
-    srv_global_window = 1 << 14
-    for setting in srv_set.payload.settings:
-        if setting.id == h2.H2Setting.SETTINGS_HEADER_TABLE_SIZE:
-            srv_hdr_tbl_sz = setting.value
-        elif setting.id == h2.H2Setting.SETTINGS_MAX_HEADER_LIST_SIZE:
-            srv_max_hdr_lst_sz = setting.value
-        elif setting.id == h2.H2Setting.SETTINGS_INITIAL_WINDOW_SIZE:
-            srv_global_window = setting.value
-
-    # 发送确认帧 将窗口定义为最大值以便于不用进行窗口管理
-    # 什么时候需要发送确认帧？？
-    set_ack = h2.H2Frame(flags={'A'}) / h2.H2SettingsFrame()
-    # 什么时候需要发送窗口更新
-    winupdate = h2.H2Frame(b'\x00\x00\x04\x08\x00\x00\x00\x00\x00\x3f\xff\x00\x01')
-    # print("-" * 32 + "ack" + "-" * 32)
-    # set_ack.show()
-    # print("-" * 32 + "ack" + "-" * 32)
-
-    # 自己的设置
-    own_set = h2.H2Frame() / h2.H2SettingsFrame()
-    max_frm_sz = (1 << 24) - 1
-    max_hdr_tbl_sz = (1 << 16) - 1
-    win_sz = (1 << 31) - 1
-    own_set.settings = [
-        h2.H2Setting(id=h2.H2Setting.SETTINGS_ENABLE_PUSH, value=0),
-        h2.H2Setting(id=h2.H2Setting.SETTINGS_INITIAL_WINDOW_SIZE, value=win_sz),
-        h2.H2Setting(id=h2.H2Setting.SETTINGS_HEADER_TABLE_SIZE, value=max_hdr_tbl_sz),
-        h2.H2Setting(id=h2.H2Setting.SETTINGS_MAX_FRAME_SIZE, value=max_frm_sz),
-    ]
-
-    h2seq = h2.H2Seq()
-    h2seq.frames = [
-        # set_ack,
-        own_set,
-    ]
-    # We verify that the server window is large enough for us to send our frames.
-    srv_global_window -= len(str(h2seq))
-    assert (srv_global_window >= 0)
-
-    print("-" * 32 + "SENDING own_set" + "-" * 32)
-    for frame in h2seq.frames:
-        frame.show()
-    sock.send(h2seq)
-    print("-" * 32 + "SENDING own_set" + "-" * 32)
-
-    # while loop for waiting until ack is received for client's settings
-    new_frame = None
-    while isinstance(new_frame, type(None)) or not (
-            new_frame.type == h2.H2SettingsFrame.type_id
-            and 'A' in new_frame.flags
-    ):
+    def send(self, dn, port, file_name, anomaly_type='normal'):
         try:
-            new_frame = sock.recv()
+            # self.seed = seed
+            self.target_addr = dn + ":" + str(port)
+            self.tls_setup_exchange(dn, port)
+            self.initial_h2_exchange()
+            frames = self.chose_frames(file_name, anomaly_type)
+            return self.send_sequence(frames)
+
+        except ConnectionRefusedError:
+            print("connection refused at {}:{}.".format(dn, port))
+
+    def chose_frames(self, file_name, anomaly_type):
+        frames_dict = load_data(file_name=file_name)
+        frames = []
+        for anomaly_name, frame_dict in frames_dict.items():
+            for frame_name, frame_fileds in frame_dict.items():
+                frame_type = extract_type(frame_name=frame_name)
+                frame = build_frame(fileds_dict=frame_fileds, frame_type=frame_type)
+                if anomaly_name == anomaly_type:
+                    frame.show()
+                    frames.append(frame)
+        # print(frames)
+        return frames
+
+    def tls_setup_exchange(self, dn, port, use_insecure_ciphers=False):
+        # 获取信息
+        # socket.getaddrinfo(host, port, family=0, type=0, proto=0, flags=0)
+        # return (family, type, proto, canonname, sockaddr)
+        # INADDR_ANY用于绑定所有借口 IPPROTO_TCP 网络层是IP 传输层是TCP
+        addr_info = socket.getaddrinfo(dn, port, socket.INADDR_ANY, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+        # family, type, proto
+        s = socket.socket(addr_info[0][0], addr_info[0][1], addr_info[0][2])
+        # set socket opt and value
+        # setsockopt(level, optname, value)
+        # level:选项所在的协议层 SOL_SOCKET：套接字层 SOL_IP：ip层 SOL_TCP：TCP层 SOL_UDP：UDP层
+        # optname SO_REUSERADDR　　　　　 允许重用本地地址和端口
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # hasattr(object, name)
+        # 判断obj是否含有name属性 并返回true false
+        if hasattr(socket, 'SO_REUSEPORT'):
+            # 复用port
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        # sockaddr
+        ip_and_port = addr_info[0][4]
+        # ssl上下文对象
+        # 选译 TLS 版本 1.2 作为通道加密协议
+        ssl_ctx = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+        # 不验证证书有效性
+        ssl_ctx.check_hostname = False
+        # 证书错误也会被忽略
+        ssl_ctx.verify_mode = ssl.CERT_NONE
+        # 不安全的加密？
+        if use_insecure_ciphers:
+            ciphers = ['AES256-GCM-SHA384', 'AES128-GCM-SHA256', 'AES256-SHA256', 'AES128-SHA256', 'CAMELLIA128-SHA256']
+        else:
+            ciphers = ['ECDHE-ECDSA-AES256-GCM-SHA384', 'ECDHE-RSA-AES256-GCM-SHA384', 'ECDHE-ECDSA-AES128-GCM-SHA256',
+                       'ECDHE-RSA-AES128-GCM-SHA256', 'ECDHE-ECDSA-AES256-SHA384', 'ECDHE-RSA-AES256-SHA384',
+                       'ECDHE-ECDSA-AES128-SHA256', 'ECDHE-RSA-AES128-SHA256', 'ECDHE-ECDSA-CAMELLIA256-SHA384',
+                       'ECDHE-RSA-CAMELLIA256-SHA384', 'ECDHE-ECDSA-CAMELLIA128-SHA256', 'ECDHE-RSA-CAMELLIA128-SHA256',
+                       'DHE-RSA-AES256-GCM-SHA384', 'DHE-RSA-AES128-GCM-SHA256', 'DHE-RSA-AES256-SHA256',
+                       'DHE-RSA-AES128-SHA256', 'AES256-GCM-SHA384', 'AES128-GCM-SHA256', 'AES256-SHA256',
+                       'AES128-SHA256', 'CAMELLIA128-SHA256']
+        # 添加加密部分
+
+        ssl_ctx.set_ciphers(':'.join(ciphers))
+
+        # 指定在 SSL/TLS 握手期间套接字应当通告的协议
+        ssl_ctx.set_alpn_protocols(['h2'])  # h2 is a RFC7540-hardcoded value
+        # ssl_ctx.set_alpn_protocols(['http / 1.1'])
+
+        # 返回的 SSL 套接字会绑定上下文、设置以及证书
+        ssl_sock = ssl_ctx.wrap_socket(s, server_hostname=dn)
+        # 这个是建立连接
+        ssl_sock.connect(ip_and_port)
+        # 检查协议是不是http2
+        print(ssl_sock.selected_alpn_protocol())
+        assert ('h2' == ssl_sock.selected_alpn_protocol())
+        scapy.config.conf.debug_dissector = True
+        ssl_stream_sock = supersocket.SSLStreamSocket(ssl_sock, basecls=h2.H2Frame)
+        self.sock = ssl_stream_sock
+
+    # 建立http2连接
+    def initial_h2_exchange(self):
+        # SENDING MAGIC
+        magic = packet.Raw(h2.H2_CLIENT_CONNECTION_PREFACE)
+        if self.verbose:
+            print("-" * 32 + "SENDING" + "-" * 32)
+            magic.show()
+        self.sock.send(magic)
+        magic.show()
+        # RECEIVING
+        srv_set = self.sock.recv()
+        srv_set.show()
+        if self.verbose:
             print("-" * 32 + "RECEIVING" + "-" * 32)
-            # 会收到什么呢？
-            new_frame.show()
-            print("-" * 32 + "RECEIVING" + "-" * 32)
-        except:
-            import time
-            time.sleep(1)
-            new_frame = None
+            srv_set.show()
+        srv_max_frm_sz = 1 << 14
+        srv_hdr_tbl_sz = 4096
+        srv_max_hdr_tbl_sz = 0
+        srv_global_window = 1 << 14
+        for setting in srv_set.payload.settings:
+            if setting.id == h2.H2Setting.SETTINGS_HEADER_TABLE_SIZE:
+                srv_hdr_tbl_sz = setting.value
+            elif setting.id == h2.H2Setting.SETTINGS_MAX_HEADER_LIST_SIZE:
+                srv_max_hdr_lst_sz = setting.value
+            elif setting.id == h2.H2Setting.SETTINGS_INITIAL_WINDOW_SIZE:
+                srv_global_window = setting.value
 
-def send_sequence(sock, frames=None):
-    if not frames:
-        print( b'no frame to send.')
-    else:
-        # 创建帧序列并且发送
-        sequence = h2.H2Seq()
-        sequence.frames = frames
-        for frame in sequence.frames:
-            frame.show()
-        sock.send(sequence)
+        srv_max_hdr_lst_sz = 1 << 10
 
-    new_frame = None
-    response_data = b''
-    status_codes = []
-    error_codes = []
+        own_set = h2.H2Frame() / h2.H2SettingsFrame()
+        max_frm_sz = (1 << 24) - 1
+        max_hdr_tbl_sz = (1 << 16) - 1
+        win_sz = (1 << 31) - 1
+        own_set.settings = [
+            h2.H2Setting(id=h2.H2Setting.SETTINGS_ENABLE_PUSH, value=0),
+            h2.H2Setting(id=h2.H2Setting.SETTINGS_INITIAL_WINDOW_SIZE, value=win_sz),
+            h2.H2Setting(id=h2.H2Setting.SETTINGS_HEADER_TABLE_SIZE, value=max_hdr_tbl_sz),
+            h2.H2Setting(id=h2.H2Setting.SETTINGS_MAX_FRAME_SIZE, value=max_frm_sz),
+        ]
 
-    while True:
-        try:
-            # new_frame是从server端接收到到信息
-            new_frame = sock.recv()
-            print("-" * 32 + "RECEIVING" + "-" * 32)
-            new_frame.show()
-            # new_frame.type == h2.H2DataFrame.type_id 判断这个帧是一个数据帧 并且有数据就加进去
-            if new_frame.type == h2.H2DataFrame.type_id and new_frame.payload:
-                response_data += new_frame.payload.data
-                # 'ES'代表END_STREAM结束
-                if 'ES' in new_frame.flags:
-                    break
-            # new_frame.type == h2.H2HeadersFrame.type_id 判断这个帧是不是头部帧
-            elif new_frame.type == h2.H2HeadersFrame.type_id and new_frame.payload:
-                status_code_in_response = False
-                # 取出状态码
-                for header in new_frame.hdrs:
-                    if 'index' in dir(header):
-                        index = int(header.index)
-                        if index >= 8 and index <= 14:
-                            status_code = bytes(h2.HPackHdrTable()._static_entries[index])
-                            status_codes.append(status_code)
+        winupdate = h2.H2Frame(b'\x00\x00\x04\x08\x00\x00\x00\x00\x00\x3f\xff\x00\x01')
+        set_ack = h2.H2Frame(flags={'A'}) / h2.H2SettingsFrame()
 
-                if 'ES' in new_frame.flags:
-                    break
-            # 判断是不是空
-            elif not isinstance(new_frame, type(None)):
-                if 'ES' in new_frame.flags or new_frame.type == h2.H2GoAwayFrame.type_id or new_frame.type == h2.H2ResetFrame.type_id:
-                    # 如果是Reset或者GoAway帧则返回错误码
-                    if new_frame.type == h2.H2ResetFrame.type_id or new_frame.type == h2.H2GoAwayFrame.type_id:
-                        error_code = str(new_frame.error).encode()
-                        error_codes.append(h2.H2ErrorCodes.literal[new_frame.getfieldval('error')].encode())
-                    break
-        except Exception as e:
-            print("ERROR")
-            import time
-            time.sleep(1)
-            new_frame = None
-    # 返回响应
-    print(b'response-code: ' + b','.join(status_codes) + b'\r\nerror: ' + b','.join(
-        error_codes)  + b'\r\n\r\n' + response_data)
+        h2seq = h2.H2Seq()
+        h2seq.frames = [
+            own_set,
+        ]
+        for frame in h2seq.frames:
+            if self.verbose:
+                print("-" * 32 + "SENDING" + "-" * 32)
+                frame.show()
+            self.sock.send(frame)
+
+        # while loop for waiting until ack is received for client's settings
+        new_frame = None
+        while isinstance(new_frame, type(None)) or not (
+                new_frame.type == h2.H2SettingsFrame.type_id
+                and 'A' in new_frame.flags
+        ):
+            try:
+                new_frame = self.sock.recv()
+                if self.verbose:
+                    print("-" * 32 + "RECEIVING" + "-" * 32)
+                    new_frame.show()
+            except:
+                import time
+                time.sleep(1)
+                new_frame = None
+
+    def send_sequence(self, frames=None):
+        if not frames:
+            return b'no frame to send.'
+        else:
+            # 创建帧序列并且发送
+            sequence = h2.H2Seq()
+            sequence.frames = frames
+            for frame in sequence.frames:
+                if self.verbose:
+                    print("-" * 32 + "SENDING" + "-" * 32)
+                    frame.show()
+            self.sock.send(sequence)
+
+        new_frame = None
+        response_data = b''
+        status_codes = []
+        error_codes = []
+
+        while True:
+            try:
+                # new_frame是从server端接收到到信息
+                new_frame = self.sock.recv()
+                if self.verbose:
+                    print("-" * 32 + "RECEIVING" + "-" * 32)
+                    new_frame.show()
+                # new_frame.type == h2.H2DataFrame.type_id 判断这个帧是一个数据帧 并且有数据就加进去
+                if new_frame.type == h2.H2DataFrame.type_id and new_frame.payload:
+                    response_data += new_frame.payload.data
+                    # 'ES'代表END_STREAM结束
+                    if 'ES' in new_frame.flags:
+                        break
+                # new_frame.type == h2.H2HeadersFrame.type_id 判断这个帧是不是头部帧
+                elif new_frame.type == h2.H2HeadersFrame.type_id and new_frame.payload:
+                    status_code_in_response = False
+                    # 取出状态码
+                    for header in new_frame.hdrs:
+                        if 'index' in dir(header):
+                            index = int(header.index)
+                            if index >= 8 and index <= 14:
+                                status_code = bytes(h2.HPackHdrTable()._static_entries[index])
+                                status_codes.append(status_code)
+
+                    if 'ES' in new_frame.flags:
+                        break
+                # 判断是不是空
+                elif not isinstance(new_frame, type(None)):
+                    if 'ES' in new_frame.flags or new_frame.type == h2.H2GoAwayFrame.type_id or new_frame.type == h2.H2ResetFrame.type_id:
+                        # 如果是Reset或者GoAway帧则返回错误码
+                        if new_frame.type == h2.H2ResetFrame.type_id or new_frame.type == h2.H2GoAwayFrame.type_id:
+                            error_code = str(new_frame.error).encode()
+                            error_codes.append(h2.H2ErrorCodes.literal[new_frame.getfieldval('error')].encode())
+                        break
+            except Exception as e:
+                _print_exception(["seed=" + str(self.seed), "host=" + str(self.target_addr)])
+                import time
+                time.sleep(1)
+                new_frame = None
+        # 返回响应
+        return b'response-code: ' + b','.join(status_codes) + b'\r\nerror: ' + b','.join(
+            error_codes) + b'\r\n\r\n' + response_data
 
 
 if __name__ == '__main__':
-    ssl_stream_sock = tls_setup_exchange(dn = '172.17.168.200', port=443)
-    initial_h2_exchange(sock=ssl_stream_sock)
-    # frames_dict = {"<headers-frame-base-2:1>": {":method": "POST", ":scheme": "https", ":path": "/", ":authority": "47.96.175.128", "flags": []},
-    #                 "<continuation-frame-pool-1:1>": {"$content-length": "5", "flags": []},
-    #                 "<continuation-frame-pool-1:2>": {"content-length": "20", "flags": []},
-    #                 "<continuation-frame-base-1:1>": {"some-header": "some-value", "flags": ["EH"]},
-    #                 "<data-frame-base-1:1>": {"data": "5\r\nCHECK\r\n0\r\n\r\n", "flags": ["ES"]}}
-    # frames = []
-    frames_dict = load_data(file_name="./caddy_attack_data.json")
-    frames = []
-    for anomaly_name, frame_dict in frames_dict.items():
-        for frame_name, frame_fileds in frame_dict.items():
-            frame_type = extract_type(frame_name=frame_name)
-            frame = build_frame(fileds_dict=frame_fileds, frame_type=frame_type)
-            print(anomaly_name)
-            frame.show()
-            frames.append(frame)
-    send_sequence(sock=ssl_stream_sock, frames=frames)
-    # for frame_name, frame_fileds in frames_dict.items():
-    #     frame_type = extract_type(frame_name=frame_name)
-    #     frame = build_frame(fileds_dict=frame_fileds, frame_type=frame_type)
-    #     frame.show()
-    #     frames.append(frame)
-    # send_sequence(sock=ssl_stream_sock,frames=frames)
+    
+    h2client = H2Client(verbose=True)
+    h2client.send(dn = '172.17.168.200', port=443,file_name="./caddy_attack_data.json", anomaly_type='no_missmatch_check_frm1')
